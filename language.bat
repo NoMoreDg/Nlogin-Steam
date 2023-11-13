7cd36484ad14bb347d2b283830c46128e9f59c1d
NoMoreDoge <61016784+NoMoreDg@users.noreply.github.com>
2023-11-13 23:46:55 +0200

    Add files via upload
    Language addon for NLoginX2V4+

---
+++ language.bat
@@ -0,0 +1,43 @@
+
+set "line=2"
+(for /L %%i in (1,1,%line%) do set /P "result=") < lgctrl.txt
+set "a1=%result%" & set "line=3"
+
+(for /L %%i in (1,1,%line%) do set /P "result=") < lgctrl.txt
+set "a2=%result%" & set "line=4"
+
+(for /L %%i in (1,1,%line%) do set /P "result=") < lgctrl.txt
+set "a3=%result%" & set "line=5"
+
+(for /L %%i in (1,1,%line%) do set /P "result=") < lgctrl.txt
+set "a4=%result%" & set "line=6"
+
+(for /L %%i in (1,1,%line%) do set /P "result=") < lgctrl.txt
+set "a5=%result%" & set "line=7"
+
+(for /L %%i in (1,1,%line%) do set /P "result=") < lgctrl.txt
+set "a6=%result%" & set "line=8"
+
+(for /L %%i in (1,1,%line%) do set /P "result=") < lgctrl.txt
+set "a7=%result%" & set "line=9"
+
+(for /L %%i in (1,1,%line%) do set /P "result=") < lgctrl.txt
+set "a8=%result%" & set "line=10"
+
+(for /L %%i in (1,1,%line%) do set /P "result=") < lgctrl.txt
+set "a9=%result%" & set "line=12"
+
+(for /L %%i in (1,1,%line%) do set /P "result=") < lgctrl.txt
+set "b1=%result%" & set "line=14"
+
+(for /L %%i in (1,1,%line%) do set /P "result=") < lgctrl.txt
+set "b2=%result%" & set "line=16"
+
+(for /L %%i in (1,1,%line%) do set /P "result=") < lgctrl.txt
+set "b3=%result%" & set "line=18"
+
+(for /L %%i in (1,1,%line%) do set /P "result=") < lgctrl.txt
+set "b4=%result%" & set "line=20"
+
+(for /L %%i in (1,1,%line%) do set /P "result=") < lgctrl.txt
+set "b5=%result%"
